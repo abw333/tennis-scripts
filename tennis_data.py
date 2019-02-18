e278524193341b629cedcfc582b766fae150d654
Alan Wagner <alanwagner333@gmail.com>
2019-02-18 15:17:04 -0400

    unzipping data from tennis-data.co.uk

--- tennis_data.py
+++ tennis_data.py
@@ -1,17 +1,19 @@
 if __name__ == '__main__':
+  import os
+  import shutil
   import sys
 
   if len(sys.argv) < 2:
     print('Missing command.')
     exit()
 
+  tennis_data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tennis-data')
+  zipped_dir = os.path.join(tennis_data_dir, 'zipped')
+  unzipped_dir = os.path.join(tennis_data_dir, 'unzipped')
+
   command = sys.argv[1]
   if command == 'download':
-    import os
-    import shutil
     import urllib.request
-
-    zipped_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tennis-data', 'zipped')
 
     if os.path.exists(zipped_dir):
       shutil.rmtree(zipped_dir)
@@ -27,5 +29,24 @@ if __name__ == '__main__':
         f'http://www.tennis-data.co.uk/{year}/{file_name}',
         os.path.join(zipped_dir, file_name)
       )
+  elif command == 'unzip':
+    import zipfile
+
+    if not os.path.exists(zipped_dir):
+      print('Must download data before unzipping it.')
+      exit()
+
+    if os.path.exists(unzipped_dir):
+      shutil.rmtree(unzipped_dir)
+
+    os.makedirs(unzipped_dir)
+
+    for year in range(2000, 2020):
+      file_name = f'{year}.zip'
+
+      print(f'Unzipping {file_name}', end='\r')
+
+      with zipfile.ZipFile(os.path.join(zipped_dir, file_name)) as z:
+        z.extractall(unzipped_dir)
   else:
     print(f'Invalid command: {command}')
